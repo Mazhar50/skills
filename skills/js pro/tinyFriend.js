475749b3128e389d189426fc0e3af9b918cb40e8
Simplilearn GitHub <Mdmifta50>
2022-10-06 13:40:35 +0600

    new 2 portfolio site inn

--- skills/js pro/tinyFriend.js
+++ skills/js pro/tinyFriend.js
@@ -1,3 +1,7 @@
 ekta array nibo 
 array er moddhe onk gula frnd er nam thakbe 
 er moddhe amake sob ceye chuto nam er frnd k output hisebe mane print korte hobe 
+
+
+
+var frndName =["mifta" "sajib" "osama " "saon" "mahin " "oni"]
