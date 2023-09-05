0c736535ed34696cfdccff3ea6d6939d78b0ece2
Mazhar50 <98317858+Mazhar50@users.noreply.github.com>
2023-09-06 00:37:03 +0600

    this is new funtion call double, saved for latter

--- shop e-commers designs/brick calculator/brickFace.js
+++ shop e-commers designs/brick calculator/brickFace.js
@@ -9,4 +9,19 @@ function myFunction() {
     } else {
         document.getElementById("total").innerHTML = (numBricks * (brick * 3));
     };
+    } 
+
+    function double() {
+        var main = 100;
+        var totalacc = document.getElementById("totalaccounts").value;
+        var a = document.getElementById("profile");
+        var profile = a.options[a.selectedIndex].value;
+        if (profile == "high") {
+            document.getElementById("total").innerHTML = (totalacc * main);            
+        }
+        else if (profile === "wide") {  document.getElementById( "total").innerHTML = (totalacc * (main * 1 ));
+        } else {
+            document.getElementById("total").innerHTML = ( totalacc * (main *2));
+            
+        };
     }
